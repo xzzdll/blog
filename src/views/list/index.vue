317692d4a73f82cf1b580116cce7e21472b87e12
John <634408262@qq.com>
2018-11-02 11:04:50 +0800

    更新ui

--- src/views/list/index.vue
+++ src/views/list/index.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <div class="ql-snow">
-        <div v-show="pageShow" class="articalCard ql-editor" :key="index" v-for="(item,index) in articalData" @click="showArtical(item._id)">
-          <div class="articalCardTitle">{{item.title}}</div>
-          <div class="articalCardSubTitle">
-            <span style="margin-right:10px">发表于:{{item.date}}</span>
-            <span style="margin-right:10px">标签:{{item.type}}</span>
-            <span style="margin-right:10px">浏览:{{item.times}}</span>
-          </div>
-          <div class="articalCardBody" v-html='item.content' v-highlight>
-            {{item.content}}
-          </div>
-          <p>....</p>
-          <div class="articalCardfoot">
-            <span>阅读全文 > ></span>
-          </div>
+    <div>
+      <div v-show="pageShow" class="articalCard" :key="index" v-for="(item,index) in articalData" @click="showArtical(item._id)">
+        <div class="articalCardTitle">{{item.title}}</div>
+        <div class="articalCardSubTitle">
+          <span style="margin-right:10px">发表于:{{item.date}}</span>
+          <span style="margin-right:10px">标签:{{item.type}}</span>
+          <span style="margin-right:10px">浏览:{{item.times}}</span>
         </div>
+        <div class="articalCardBody" v-html='item.content' v-highlight>
+          {{item.content}}
+        </div>
+        <div>....</div>
+        <div class="articalCardfoot">
+          <span>阅读全文 > ></span>
+        </div>
+      </div>
       <el-pagination v-show="pageShow" :small=true @size-change="handleSizeChange" @current-change="handleCurrentChange" class="z-pagination" :current-page.sync="currentPage" :page-size="pageSize" layout="sizes,prev, pager, next" :total="totalRows">
       </el-pagination>
     </div>
@@ -90,9 +90,10 @@ export default {
   cursor: pointer;
 
   .articalCardTitle {
-    margin-bottom: 5px;
-    font-size: 25px;
-    font-weight: bold;
+    margin-bottom: 10px;
+    font-size: 26px !important;
+    font-weight: 400 !important;
+    color: rgb(85, 85, 85) !important;
   }
 
   .articalCardSubTitle {
@@ -103,21 +104,24 @@ export default {
 
   .articalCardBody {
     width: 100%;
-    margin-bottom: 8px;
+    margin-bottom: 0px;
     text-align: left;
+    font-size: 14px !important;
+    color: rgb(85, 85, 85) !important;
   }
 
   .articalCardfoot {
-    color: #108ee9;
+    color: rgb(85, 85, 85);
     font-weight: 600;
+    border-bottom: 2px solid rgb(102, 102, 102);
   }
 }
 </style>
 
 <style lang="scss">
 // .ql-snow .ql-editor pre.ql-syntax {
-  // background-color: #f7f7f7;
-  // // background-color: #23241f;
-  // color: #000;
+// background-color: #f7f7f7;
+// // background-color: #23241f;
+// color: #000;
 // }
 </style>
